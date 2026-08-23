5bcf5ba3a0ccd79b1470ccebe709f1f3c59b2000
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-23 17:16:44 +0300

    sync

--- src/scipy/demos/scipy_stats.py
+++ src/scipy/demos/scipy_stats.py
@@ -5,17 +5,17 @@ TBD
 """
 
 import matplotlib.pyplot as plt
+import scipy.stats
 
 import numpy as np
-from scipy import stats
 
 s = np.r_
 
-rv1 = stats.norm()
-rv2 = stats.norm(2.0, 0.8)
+rv1 = scipy.stats.norm()
+rv2 = scipy.stats.norm(2.0, 0.8)
 samp = s[rv1.rvs(size=100), rv2.rvs(size=100)]
 # Kernel estimate (smoothed histogram)
-apdf = stats.kde.gaussian_kde(samp)
+apdf = scipy.stats.kde.gaussian_kde(samp)
 x = np.linspace(-3, 6, 200)
 plt.plot(x, apdf(x), 'r')
 
