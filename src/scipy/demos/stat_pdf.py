ac3694c09d39c3f6197f705ee012d41d9e2acacb
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-23 17:03:37 +0300

    sync

--- src/scipy/demos/stat_pdf.py
+++ src/scipy/demos/stat_pdf.py
@@ -7,8 +7,8 @@ TBD
 import matplotlib.pyplot as plt
 import scipy.stats as scs
 
-import scipy as sc
+import numpy as np
 
-x = scs.norm.pdf(sc.r_[-5:5:100])
+x = scs.norm.pdf(np.r_[-5:5:100])
 plt.plot(x)
 plt.show()
